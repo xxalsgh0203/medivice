79ecd45f98cf2763559e155f1e869b9f22697556
Minho Kim <music4537@naver.com>
2021-12-03 13:55:41 -0600

    medivice

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'tailwind-rn';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={tw["flex-1 justify-center items-center"]}>
+      <Text>This is a start of a Medicide app</Text>
+      <Button title="click me" />
       <StatusBar style="auto" />
     </View>
   );
